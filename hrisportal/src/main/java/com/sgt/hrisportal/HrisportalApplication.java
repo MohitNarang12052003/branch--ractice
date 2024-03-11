19256f5582ffd1953c23917c4e4dcd17b22f2d76
MohitNarang12052003 <mohitnarang2003.mn@gmail.com>
2024-03-11 18:21:26 +0530

    first-commit

---
+++ hrisportal/src/main/java/com/sgt/hrisportal/HrisportalApplication.java
@@ -0,0 +1,13 @@
+package com.sgt.hrisportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrisportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrisportalApplication.class, args);
+	}
+
+}
